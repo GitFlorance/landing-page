61ee7ccc58706144d445fd92d741aaeac96d4f27
Suembike <Simochka.violet@mail.ru>
2021-12-02 17:45:35 +0300

    add animation of the menu button

--- build/js/index.min.js
+++ build/js/index.min.js
@@ -3,10 +3,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const startButton = document.querySelector(".js-menu_button");
     const menu = document.querySelector(".menu");
     const body = document.querySelector("body");
+    const lineOne = document.querySelector(".js-menu_lineOne");
+    const lineTwo = document.querySelector(".js-menu_lineTwo");
 
     startButton.addEventListener ("click", (event) => {
         menu.classList.toggle('invisible');
         body.classList.toggle('hidden');
+        lineOne.classList.toggle('line__1');
+        lineTwo.classList.toggle('line__2');
         console.log("f");
     });
 });
